84a28cd14cb345d8cfafd10f09ef68bb02877234
Hassane Dao <dao.hassane@gmail.com>
2021-01-04 09:51:02 +0000

    chore(): add answers

--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,58 @@
 import arg from 'arg';
 
+/**
+ *
+ * @param rawArgs
+ * @returns {{template: string, runInstall: (boolean|boolean)}}
+ */
+function parseArgumentsIntoOptions(rawArgs) {
+    const args = arg(
+        {
+            '--install': Boolean,
+            '--yes': Boolean,
+            '-i': Boolean,
+        },
+        {
+            argv: rawArgs.slice(2)
+        },
+    );
+
+    return {
+        skipPrompts: args['--yes'] || false,
+        template: args._[0],
+        runInstall: args['--install'] || false,
+    };
+}
+
+async function promptForMissingOptions(options) {
+    const defaultTemplate = 'Simple';
+
+    if (options.skipPrompts) {
+        return {
+            ...options,
+            template: options.template || defaultTemplate
+        };
+    }
+
+    /**
+     *
+     * @type {*[]}
+     */
+    let questions = [];
+
+    if (!options.template) {
+        questions.push({
+            type: 'list',
+            name: 'template',
+            message: 'Please choose which project template to use',
+            choices: ['Simple', 'SQRS'],
+            default: defaultTemplate
+        })
+    }
+}
+
+
 export function cli(args) {
-    console.log(args);
+    let options = parseArgumentsIntoOptions(args);
+    console.log(options);
 }
